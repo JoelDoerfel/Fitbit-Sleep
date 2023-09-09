00411ef57109b2ff0a2cecd32587ad1bba955f21
Joel Doerfel <jddoerfel@gmail.com>
2023-09-09 07:05:00 -0700

    Update fitbitSleep.py

--- fitbitSleep.py
+++ fitbitSleep.py
@@ -9,8 +9,11 @@ import fitbit
 import gather_keys_oauth2 as Oauth2
 import pandas as pd 
 import datetime
-CLIENT_ID = 'YOUR-CLIENT-ID'
-CLIENT_SECRET = 'YOUR-CLIENT-SECRET'
+
+from dev_secrets.fitbit_creds import fitbit_client_id, fitbit_client_secret
+
+CLIENT_ID = fitbit_client_id
+CLIENT_SECRET = fitbit_client_secret
 
 server = Oauth2.OAuth2Server(CLIENT_ID, CLIENT_SECRET)
 server.browser_authorize()
@@ -23,40 +26,59 @@ yesterday2 = str((datetime.datetime.now() - datetime.timedelta(days=1)).strftime
 today = str(datetime.datetime.now().strftime("%Y%m%d"))
 today2 = str(datetime.datetime.now().strftime("%Y-%m-%d"))
 
-"""
-fit_statsHR = auth2_client.intraday_time_series('activities/heart', base_date=yesterday2, detail_level='1sec')
+def intraday_hr(file_path):
+    fit_statsHR = auth2_client.intraday_time_series('activities/heart', base_date=yesterday2, detail_level='1sec')
 
-time_list = []
-val_list = []
-for i in fit_statsHR['activities-heart-intraday']['dataset']:
-    val_list.append(i['value'])
-    time_list.append(i['time'])
-heartdf = pd.DataFrame({'Heart Rate':val_list,'Time':time_list})
+    date_list = []
+    time_list = []
+    val_list = []
+    for i in fit_statsHR['activities-heart-intraday']['dataset']:
+        date_list.append(str(yesterday2))
+        val_list.append(i['value'])
+        time_list.append(i['time'])
+    heartdf = pd.DataFrame({'Date':date_list,'Time':time_list,'Heart Rate':val_list})
 
-heartdf.to_csv('C:/{YOUR PATH}/Heart/heartrate-'+ \
-                 yesterday+'.csv', \
-                 columns=['Time','Heart Rate'], header=True, \
-                 index = False)
-"""
+    folder = 'Intraday_HR/'
+    file_name = 'intraday_hr_'+yesterday+'.csv'
+    heartdf.to_csv(file_path+folder+file_name, \
+                    columns=['Date','Time','Heart Rate'], header=True, \
+                    index = False)
 
-fit_statsSl = auth2_client.sleep(date='today')
+    #return heartdf                    
 
-my_data = fit_statsSl
-json_data = json.dumps(my_data)
-print(json_data)
+def sleep_stages(file_path):
+    fit_statsSl = auth2_client.sleep(date='today')
 
-sdate_list = []
-slevel_list = []
-ssec_list = []
+    my_data = fit_statsSl
+    json_data = json.dumps(my_data)
+    print(json_data)
 
-for i in fit_statsSl['sleep'][0]['levels']['data']:
-    sdate_list.append(i['dateTime'])
-    slevel_list.append(i['level'])
-    ssec_list.append(i['seconds'])
-sleepdf = pd.DataFrame({'Time':sdate_list,
-                     'Level':slevel_list,
-                     'Duration':ssec_list})
-sleepdf.to_csv('C:/{YOUR PATH}/SleepStage/sleepStages-' + \
-               today+'.csv', \
-               columns = ['Time','Level','Duration'],header=True, 
-               index = False)
+    sdate_list = []
+    slevel_list = []
+    ssec_list = []
+
+    for i in fit_statsSl['sleep'][0]['levels']['data']:
+        sdate_list.append(i['dateTime'])
+        slevel_list.append(i['level'])
+        ssec_list.append(i['seconds'])
+
+    sleepdf = pd.DataFrame({'Time':sdate_list,
+                        'Level':slevel_list,
+                        'Duration':ssec_list})
+
+    folder = 'Sleep_Stages/'
+    file_name = 'sleepStages_'+yesterday+'.csv'
+
+    sleepdf.to_csv(file_path+folder+file_name, 
+                columns = ['Date','Time','Level','Duration'], 
+                header=True, 
+                index = False)
+
+    #return sleepdf
+
+def main():
+    file_path = 'C:/Users/Admin/Documents/Fitbit/'
+    intraday_hr(file_path)
+    #sleep_stages(file_path)
+
+main()
